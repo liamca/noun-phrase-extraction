a459dc31d919226023ecc481c563f1faacc60866
Liam Cavanagh <liamca@users.noreply.github.com>
2022-11-11 08:47:00 -0800

    Update npe.py

--- npe.py
+++ npe.py
@@ -1,25 +1,17 @@
+# !python -m spacy download en_core_web_sm
 import string
-from nltk import word_tokenize, pos_tag, ne_chunk, sent_tokenize
-from nltk import RegexpParser, Tree
+import spacy
+nlp = spacy.load("en_core_web_sm")
 from nltk.stem import WordNetLemmatizer  
 from nltk.corpus import stopwords
-
-# Defining a grammar & Parser
-# This one seems to be quite good for medical content
-# NP = "NP: {<NN.?>+<DT.?>+|<JJ.?>*<NN.?>+<IN.?|DT.?|JJ.?|NN.?>+<NN.?>{1}|<NN.?>+|<JJ.?>+<NN.?>+}"
-# This one is for continuous nouns
-NP = "NP: {<NN.?>+}"
-# This one is for continuous nouns preceded by an adjective
-# NP = "NP: {<JJ.?|NN.?>+}"
-
+from nltk import word_tokenize, sent_tokenize
 lemmatizer = WordNetLemmatizer()
 stop = set(stopwords.words('english') + list(string.punctuation))
-chunker = RegexpParser(NP)
 
 must_contain_alphe = True
 min_term_len = 3
 
-def extract_noun_phrases(text):
+def noun_phrase_by_sentence(text):
     tokenized_content = []
 
     sentences = []
@@ -30,44 +22,30 @@ def extract_noun_phrases(text):
                     sentences.append(sss.strip())
 
 
+    np_list = []
     for sentence in sentences:
-        # print (sentence)
-        tokenized = word_tokenize(sentence.lower())
-        sent_stop = [i for i in  tokenized if i not in stop]
-        # print ('sent_stop', sent_stop)
-        cleaned_sent_stop = []
-        for ss in sent_stop:
-            if len(ss) >= min_term_len and any(c.isalpha() for c in ss):
-                cleaned_sent_stop.append(ss)
+        np_sentence_list = []
+        doc = nlp(sentence)
+        for noun_chunk in doc.noun_chunks:
+            sent_stop = [i for i in word_tokenize(noun_chunk.text.lower()) if i not in stop]
+            # print ('sent_stop', sent_stop)
+            cleaned_sent_stop = []
+            for ss in sent_stop:
+                if len(ss) >= min_term_len and any(c.isalpha() for c in ss):
+                    cleaned_sent_stop.append(ss)
 
-        # print ('cleaned_sent_stop', cleaned_sent_stop)
-        lemmatized = []
-        if len(cleaned_sent_stop) > 0:
-            lemmatized=[lemmatizer.lemmatize(word) for word in cleaned_sent_stop]
-        # print ('lemmatized', lemmatized)
-        continuous_chunk = []
-        current_chunk = []
-        if len(cleaned_sent_stop) > 0:
-            chunked = chunker.parse(pos_tag(cleaned_sent_stop))
-            for subtree in chunked:
-                if type(subtree) == Tree:
-                    for token, pos in subtree:
-                        if token.isalnum() and len(token) > 1:
-                            # print (token)
-                            lemma_token = lemmatizer.lemmatize(token)
-                            current_chunk.append(lemma_token)
-                            # print (lemma_token)
+            # print ('cleaned_sent_stop', cleaned_sent_stop)
+            lemmatized = []
+            if len(cleaned_sent_stop) > 0:
+                lemmatized=[lemmatizer.lemmatize(word) for word in cleaned_sent_stop]
 
-                    if len(current_chunk) > 0:
-                        continuous_chunk.append("_".join(current_chunk))
-                        current_chunk = []
+            if len(lemmatized) > 0:
+                # print ('lemmatized', lemmatized)
+                for n in lemmatized:
+                    np_sentence_list.append(n)
+                np_sentence_list.append("_".join(lemmatized))
 
-        # Remove the chnunks that are already in sentence
-        continuous_chunk_cleaned = []
-        for chunk in continuous_chunk:
-            if chunk not in lemmatized:
-                continuous_chunk_cleaned.append(chunk)
-        continuous_chunk_cleaned = lemmatized + continuous_chunk_cleaned
-        tokenized_content.append(list(set(continuous_chunk_cleaned)))            
-
-    return tokenized_content
+        if len(np_sentence_list) > 0:
+            np_list.append(list(set(np_sentence_list)))
+            # print('np_sentence_list', np_sentence_list)
+    return np_list
